12866d121d593600e52327dc15ba920d9592e239
Shreyas S <64488662+shreyassp17@users.noreply.github.com>
2024-03-13 10:11:22 +0530

    Change currency in CartOverview

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -12,7 +12,7 @@ function CartOverview() {
     <div className="flex items-center justify-between bg-stone-800 text-stone-200 text-sm uppercase px-4 py-4 sm:px-6 md:text-base ">
       <p className="font-semibold text-stone-300 space-x-4 sm:space-x-6">
         <span>{totalCartQuantity} pizzas</span>
-        <span>${totalCartPrice}</span>
+        <span>€{totalCartPrice}</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
     </div>
